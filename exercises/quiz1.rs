567358b0835cd002870cf63227fd2f74fb734612
Oguzhan <oguzhan.kaluk@gmail.com>
2023-07-31 20:40:07 +0200

    finished for today

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(input:i32) -> i32  {
+    let mut price = 2;
+    if input > 40  {
+        price = 1
+    }
+
+    price * input
+}
 
 // Don't modify this function!
 #[test]
